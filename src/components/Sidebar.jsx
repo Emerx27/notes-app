f6b291a3132f61b589830232674e96f1bf718545
Emerson Díaz <emedzh@gmail.com>
2025-01-17 16:50:45 -0600

    feat(Sidebar): edit note in real time functionality
    - implemented noteEditedId function to save the id of the note
    - implemented editNote function to create a form to edit the note
    - implemented fillInput function to edit the note in real time

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,38 +3,59 @@ let nextId = 1;
 
 function Sidebar() {
     const [allNotes, setAllNotes] = useState([]);
+    const [currentId, setCurrentId] = useState(null)
     const [note, setNote] = useState({
         id: 1,
         title: "Title",
-        tags: ["Thoughts"],
         content: "Write your thoughts here...",
     });
 
     function createNote() {
         const day = new Date().getDate();
         let month = new Date().getMonth() + 1;
-        if(month < 10) {
+        if (month < 10) {
             month = `0${month}`;
         }
         const year = new Date().getFullYear();
         const date = `${day} / ${month} / ${year}`;
-        const newNote = { ...note, id: nextId, date};
+        const newNote = { ...note, id: nextId, date };
         setAllNotes([...allNotes, newNote]);
         nextId++;
+        setCurrentId(allNotes.length + 1);
     }
 
     function renderTask() {
         return allNotes.map(note => (
             <li key={note.id}>
-                <article>
+                <article onClick={() => noteEditedId(note.id)}>
                     <h3>{note.title}</h3>
-                    {note.tags.map(tag => <p key={note.id}>{tag}</p>)}
                     <p>Created: {note.date}</p>
                 </article>
             </li>
         ));
     }
 
+    function noteEditedId(id) {
+        setCurrentId(id);
+    }
+
+    function editNote() {
+        const actualEditedNote = allNotes.filter(note => note.id === currentId);
+        return actualEditedNote.map(note => (
+            <form key={note.id}>
+                <input type="text" value={note.title} onChange={fillInput} name="title" />
+
+                <textarea value={note.content} onChange={fillInput} name="content"></textarea>
+            </form>
+        ))
+    }
+
+    function fillInput(e) {
+        const { name, value } = e.target;
+        setAllNotes(prevNotes => prevNotes.map(note =>
+            note.id === currentId ? { ...note, [name]: value } : note
+        ));
+    }
     return (
         <aside>
             <button onClick={createNote}>Create new note</button>
@@ -42,6 +63,8 @@ function Sidebar() {
             <ul>
                 {renderTask()}
             </ul>
+
+            {editNote()}
         </aside>
     )
 }
