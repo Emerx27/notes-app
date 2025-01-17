2a06c47d16f064f6f6df76ca074704de32800925
Emerson Díaz <emedzh@gmail.com>
2025-01-17 12:43:35 -0600

    feat(Sidebar): add createNote function to handle note creation
    - Added createNote function to create new notes.
    - Implemented useState hook to store created notes.
    - createNote is executed when the button is clicked.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 function Sidebar() {
+    const [allNotes, setAllNotes] = useState([]);
+    const [note, setNote] = useState({
+        id: 1,
+        title: "Title",
+        tags: [],
+        content: "Write your thoughts here..."
+    });
+
+    function createNote() {
+        const newNote = { ...note, id: note.id + 1};
+        setAllNotes([...allNotes, newNote]);
+    }
+    
     return (
         <aside>
-            <button>Create new note</button>
+            <button onClick={createNote}>Create new note</button>
         </aside>
     )
 }
