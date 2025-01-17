c001cc8b3f2af0c01685845fb0fa9ab333e3080a
Emerson Díaz <emedzh@gmail.com>
2025-01-17 12:19:12 -0600

    feat: add Sidebar component
    - Created Sidebar component structure in the components folder.
    - Imported Sidebar component into App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
 
 function App() {
   return (
     <>
       <Header/>
+      <main>
+        <Sidebar/>
+      </main>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+function Sidebar() {
+    return (
+        <aside>
+            <button>Create new note</button>
+        </aside>
+    )
+}
+
+export default Sidebar;
