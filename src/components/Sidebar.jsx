391940f0235df454c8a16aac671bb45a00715e84
Emerson Díaz <emedzh@gmail.com>
2025-01-17 13:28:13 -0600

    feat(Sidebar): implemented function to render tasks with creation date
    - renderTask render all tasks in the sidebar.
    - added creation date of the task.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,19 +6,42 @@ function Sidebar() {
     const [note, setNote] = useState({
         id: 1,
         title: "Title",
-        tags: [],
-        content: "Write your thoughts here..."
+        tags: ["Thoughts"],
+        content: "Write your thoughts here...",
     });
 
     function createNote() {
-        const newNote = { ...note, id: nextId};
+        const day = new Date().getDate();
+        let month = new Date().getMonth() + 1;
+        if(month < 10) {
+            month = `0${month}`;
+        }
+        const year = new Date().getFullYear();
+        const date = `${day} / ${month} / ${year}`;
+        const newNote = { ...note, id: nextId, date};
         setAllNotes([...allNotes, newNote]);
         nextId++;
     }
-    
+
+    function renderTask() {
+        return allNotes.map(note => (
+            <li key={note.id}>
+                <article>
+                    <h3>{note.title}</h3>
+                    {note.tags.map(tag => <p key={note.id}>{tag}</p>)}
+                    <p>Created: {note.date}</p>
+                </article>
+            </li>
+        ));
+    }
+
     return (
         <aside>
             <button onClick={createNote}>Create new note</button>
+
+            <ul>
+                {renderTask()}
+            </ul>
         </aside>
     )
 }
