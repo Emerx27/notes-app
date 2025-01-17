3d2c88f37258e5163f54a89d724f273161a870c1
Emerson Díaz <emedzh@gmail.com>
2025-01-17 12:03:55 -0600

    feat: add Header component
    - folder components created on src.
    - Created Header component structure in the components folder.
    - Imported Header component into App.jsx.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <div>
+                <h1>Notes</h1>
+
+                <input type="text" placeholder="Search by title, content, or tags..."/>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
