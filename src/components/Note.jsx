d9aa4a371499297e527b8877aad4fa6aede4ce5d
Emerson Díaz <emedzh@gmail.com>
2025-01-27 14:11:54 -0600

    fix(Note): fix onClick event

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
-function Note({allNotes, noteEditedId, filterValue, setIsEdited}) {
+function Note({ allNotes, noteEditedId, filterValue, setIsEdited }) {
     const filteredNotes = allNotes.filter(note => note.title.toLowerCase().includes(filterValue));
     return filteredNotes.map(note => (
-        <li className="sidebar__list-note" key={note.id}>
-            <article className="sidebar__list-note-content" onClick={() => {
-                noteEditedId(note.id)
-                setIsEdited(true)
-                }}>
+        <li className="sidebar__list-note" key={note.id} onClick={() => {
+            noteEditedId(note.id)
+            setIsEdited(true)
+        }}>
+            <article className="sidebar__list-note-content">
                 <h3 className="sidebar__list-note-title">{note.title}</h3>
                 <p className="sidebar__list-note-date">Created: {note.date}</p>
             </article>
