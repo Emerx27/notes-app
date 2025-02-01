8d9a9e983aacd6c70a8405726aa867e188186d94
Emerson Díaz <emedzh@gmail.com>
2025-02-01 12:26:31 -0600

    feat(Menu): added scss styles
    - added scss styles on scss/base/layout/_header.scss

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,17 @@
 function Menu() {
     return (
         <div className="header__menu">
-            <span className="header__menu-icon header__menu-icon--dark material-symbols-outlined">
-                light_mode
-            </span>
-            
-            <span className="header__menu-switch"></span>
+            <div className="header__menu-content">
+                <span className="header__menu-icon header__menu-icon--dark material-symbols-outlined">
+                    light_mode
+                </span>
 
-            <span className="header__menu-icon header__menu-icon--dark material-symbols-outlined">
-                dark_mode
-            </span>
+                <span className="header__menu-switch"></span>
+
+                <span className="header__menu-icon header__menu-icon--dark material-symbols-outlined">
+                    dark_mode
+                </span>
+            </div>
         </div>
     )
 }
